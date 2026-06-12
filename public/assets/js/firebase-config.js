3677be2767bcc9842d260a7b9e16203d382c2bf9
Tom09s <chpalitom09@gmail.com>
2026-06-12 17:26:24 +0200

    Update firebase-config.js

--- public/assets/js/firebase-config.js
+++ public/assets/js/firebase-config.js
@@ -1,24 +1,27 @@
 // Firebase Configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyD6qIOtKSx0Sl1Ht_a6ppLiMdKZRCc75tA",
-    authDomain: "wikimind-3-comments.firebaseapp.com",
-    databaseURL: "https://wikimind-3-comments-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "wikimind-3-comments",
-    storageBucket: "wikimind-3-comments.firebasestorage.app",
-    messagingSenderId: "137479541640",
-    appId: "1:137479541640:web:4ed2510a9908055052e03d"
+    apiKey: "AIzaSyB5BwEC69LUaPGtmLz4fuuD_jPXpB7B7mE",
+    authDomain: "wikimind-food-api.firebaseapp.com",
+    databaseURL: "https://wikimind-food-api-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "wikimind-food-api",
+    storageBucket: "wikimind-food-api.firebasestorage.app",
+    messagingSenderId: "1045464204780",
+    appId: "1:1045464204780:web:c17ed616c161a5482c2cc9",
+    measurementId: "G-W2F98GE2PS"
 };
 
 // Initialize Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-analytics.js";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-auth.js";
 import { getDatabase, ref, set, get, push, remove, onValue, query, orderByChild, limitToLast } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-database.js";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const database = getDatabase(app);
 const googleProvider = new GoogleAuthProvider();
 
 // Export Firebase services
-export { auth, database, googleProvider, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged, ref, set, get, push, remove, onValue, query, orderByChild, limitToLast };
+export { auth, database, googleProvider, analytics, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged, ref, set, get, push, remove, onValue, query, orderByChild, limitToLast };
